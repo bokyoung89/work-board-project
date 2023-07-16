70ca78007a8969a31a2a03b23767aa744a608631
bokyoung shin <sbk8689@gmail.com>
2023-07-16 23:06:55 +0900

    Init : #6 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    들어간 초기 기술은 :
    * 스프링 웹
    * 엑츄에이터
    * 데브툴즈
    * 롬복
    
    .gitignore 적용
    추가 제외한 파일은 :
    * java
    * Gradle
    * intelliJ all
    * Windows

---
+++ src/test/java/com/bokyoung/workboardproject/WorkBoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bokyoung.workboardproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkBoardProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
