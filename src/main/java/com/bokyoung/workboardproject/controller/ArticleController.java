99a11879d370248ac362d9be16c5f4a57ded2717
bokyoung shin <sbk8689@gmail.com>
2023-07-30 00:55:22 +0900

    #16 - 게시글 관련 뷰 테스트 작성
    우선 어떤 경로로 뷰를 열어줄지 테스트로 명세를 잡는다.

---
+++ src/main/java/com/bokyoung/workboardproject/controller/ArticleController.java
@@ -0,0 +1,9 @@
+package com.bokyoung.workboardproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+}
