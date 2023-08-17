f255b092e13843d13facb14294141356eb07476f
bokyoung shin <sbk8689@gmail.com>
2023-08-17 13:47:10 +0900

    refactor : # 39 -  FormStatus 값 변경
     CREATE 한글명을 저장에서 등록으로 수정함

--- src/main/java/com/bokyoung/workboardproject/domain/constant/FormStatus.java
+++ src/main/java/com/bokyoung/workboardproject/domain/constant/FormStatus.java
@@ -3,7 +3,7 @@ package com.bokyoung.workboardproject.domain.constant;
 import lombok.Getter;
 
 public enum FormStatus {
-    CREATE("저장", false),
+    CREATE("등록", false),
     UPDATE("수정", true);
 
     @Getter
